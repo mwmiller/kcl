d44f25c1ad014578b19339563f7aee72d09b2f8c
Matt Miller <mwm@fastmail.com>
2016-05-16 12:07:12 +0200

    Update dependencies.

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Kcl.Mixfile do
       {:salsa20, "~> 0.3"},
       {:poly1305, "~> 0.4"},
       {:power_assert, "~> 0.0.8", only: :test},
-      {:earmark, ">= 0.0.0", only: :dev},
+      {:earmark, "~> 0.2", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev},
     ]
   end
