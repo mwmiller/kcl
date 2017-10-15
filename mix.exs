4c4d874587ead2a2d2138895216a9c0f7d2079f3
Matt Miller <mwm@fastmail.com>
2017-10-15 09:00:29 +0200

    Increment to 1.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Kcl.Mixfile do
 
   def project do
     [app: :kcl,
-     version: "1.0.1",
+     version: "1.0.2",
      elixir: "~> 1.4",
      name: "KCl",
      source_url: "https://github.com/mwmiller/kcl",
@@ -20,12 +20,12 @@ defmodule Kcl.Mixfile do
 
   defp deps do
     [
-      {:ed25519, "~> 1.0"},
+      {:ed25519, "~> 1.1"},
       {:curve25519, "~> 1.0"},
       {:salsa20, "~> 1.0"},
       {:poly1305, "~> 1.0"},
       {:earmark, "~> 1.0", only: :dev},
-      {:ex_doc, "~> 0.14", only: :dev},
+      {:ex_doc, "~> 0.18", only: :dev},
       {:credo, "~> 0.8", only: [:dev, :test]},
     ]
   end
