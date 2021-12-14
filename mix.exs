dccd9dac96d7632e6b672794f5ba5c45ab379ed3
Matthew Miller <matt.miller@nfiindustries.com>
2021-12-14 23:02:41 +0200

    Increment version for hex release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kcl.Mixfile do
   def project do
     [
       app: :kcl,
-      version: "1.4.0",
+      version: "1.4.1",
       elixir: "~> 1.9",
       name: "KCl",
       source_url: "https://github.com/mwmiller/kcl",
