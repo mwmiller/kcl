1e8b4d54f629245dc22d2308c9473e4d1d8430d9
Matthew Miller <mwm@fastmail.com>
2026-04-05 11:25:23 -0400

    Use fadter ed25519

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kcl.MixProject do
   def project do
     [
       app: :kcl,
-      version: "1.5.0",
+      version: "1.5.1",
       elixir: "~> 1.9",
       name: "KCl",
       source_url: "https://github.com/mwmiller/kcl",
